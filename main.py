fc2234630373a92d8d441d2e179eff5db641ee40
ymbrael <ymbrael@proton.me>
2024-10-28 22:21:56 -0700

    added screen

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import pygame
 
+pygame.init()
+
 from constants import *
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    
 def main():
     print(
         "Starting asteroids!\n"
         f"Screen width: {SCREEN_WIDTH}\n"
         f"Screen height: {SCREEN_HEIGHT}\n"
     )
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("#002000")
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
